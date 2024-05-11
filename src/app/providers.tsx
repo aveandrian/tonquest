53647e599c5204635a60c90b6019ea227f07e6c7
Andrei Andrianov <trancemilly7@gmail.com>
2024-05-12 00:52:10 +0200

    manifest update

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -10,7 +10,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 export function Providers(props: { children: ReactNode }) {
   return (
     <WagmiProvider config={config}>
-      <TonConnectUIProvider manifestUrl="https://twitter-clone-beta-beryl.vercel.app/tonconnect-manifest.json">
+      <TonConnectUIProvider manifestUrl="https://tonquest.vercel.app/tonconnect-manifest.json">
         {props.children}
       </TonConnectUIProvider>
     </WagmiProvider>
