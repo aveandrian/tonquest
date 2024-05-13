aa2dff64e5549a5fdaa001ddae83d98e79bc841e
Andrei Andrianov <trancemilly7@gmail.com>
2024-05-13 21:58:23 +0200

    adds webapp

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,18 +7,21 @@ import { config } from "@/wagmi";
 
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
+import { TelegramProvider } from "./TelegramProvider";
 
 export function Providers(props: { children: ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <TonConnectUIProvider manifestUrl="https://tonquest.vercel.app/tonconnect-manifest.json">
-        {props.children}
-        <ProgressBar
-          height="4px"
-          color="#F85525"
-          options={{ showSpinner: false }}
-          shallowRouting
-        />
+        <TelegramProvider>
+          {props.children}
+          <ProgressBar
+            height="4px"
+            color="#F85525"
+            options={{ showSpinner: false }}
+            shallowRouting
+          />
+        </TelegramProvider>
       </TonConnectUIProvider>
     </WagmiProvider>
   );
