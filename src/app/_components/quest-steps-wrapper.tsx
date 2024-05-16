60118c83e82742372f590c2f5f3542ed5100f284
Andrei Andrianov <trancemilly7@gmail.com>
2024-05-16 21:40:27 +0200

    adds steps animation

--- src/app/_components/quest-steps-wrapper.tsx
+++ src/app/_components/quest-steps-wrapper.tsx
@@ -8,6 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import { Spinner } from "@nextui-org/react";
 import { CheckmarkAnimation } from "./checkmark-animation";
+import { motion } from "framer-motion";
 
 export function QuestStepsWrapper({
   stepsInfo,
@@ -81,17 +82,24 @@ export function QuestStepsWrapper({
           stepsInfo.map((step, i) => (
             <div
               key={step.step_id}
-              className={`flex grid-cols-1 items-center justify-between rounded-md border-2 border-solid p-2 ${currentStepInfo?.step_order === i ? "border-sandyBrown bg-sandyBrown" : "border-peachYellow bg-peachYellow"}`}
+              className="relative flex grid-cols-1 items-center justify-between rounded-md border-2 border-solid border-peachYellow bg-peachYellow p-2"
             >
-              <p>{step.step_title}</p>
+              <p className="z-10">{step.step_title}</p>
               {userQuestProgress &&
                 i <= userQuestProgress?.current_step_order && (
                   <FontAwesomeIcon
                     icon={faCheckCircle}
                     size="lg"
                     color="teal"
+                    className="z-10"
                   />
                 )}
+              {currentStepInfo?.step_order === i ? (
+                <motion.div
+                  className="absolute left-0 top-0 h-full w-full overflow-hidden rounded-md border-2 border-solid border-sandyBrown bg-sandyBrown"
+                  layoutId="underline"
+                />
+              ) : null}
             </div>
           ))}
       </div>
